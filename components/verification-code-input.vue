de66bfdbf1c8a2f7694c61b44c52382d855df559
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-05-26 20:31:59 -0400

    build: added quick paste for code

--- components/verification-code-input.vue
+++ components/verification-code-input.vue
@@ -4,14 +4,13 @@
             <input v-for="(digit, index) in verificationCodeDigits" :key="index" type="text"
                 v-model="verificationCodeDigits[index]" maxlength="1" required
                 class="w-12 h-12 text-center text-2xl border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-                @input="handleDigitInput(index)" @keydown.delete="handleDeleteKey(index)" />
+                @input="handleDigitInput(index)" @keydown.delete="handleDeleteKey(index)" @paste="handlePaste" />
         </div>
         <button type="submit" class="w-full bg-black text-white font-bold py-2 px-4 rounded-md hover:bg-gray-800">
             Verify
         </button>
     </form>
 </template>
-
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 
@@ -31,15 +30,27 @@ const handleDeleteKey = (index: number) => {
     }
 };
 
+const handlePaste = (event: ClipboardEvent) => {
+    const pasteData = event.clipboardData?.getData('text');
+    if (pasteData) {
+        event.preventDefault();
+        verificationCodeDigits.value = pasteData.substring(0, 6).split('');
+    }
+};
+
 const submitVerificationCode = () => {
     const code = verificationCodeDigits.value.join('');
     emit('submit', code);
 };
 
-watch(verificationCodeDigits, (newValue) => {
-    const isAllDigitsFilled = newValue.every((digit) => digit !== '');
-    if (isAllDigitsFilled) {
-        submitVerificationCode();
-    }
-}, { deep: true });
+watch(
+    verificationCodeDigits,
+    (newValue) => {
+        const isAllDigitsFilled = newValue.every((digit) => digit !== '');
+        if (isAllDigitsFilled) {
+            submitVerificationCode();
+        }
+    },
+    { deep: true }
+);
 </script>
