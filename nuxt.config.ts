067d186d754dd7cf2bde46a9eba272a14cd0a8c4
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-05-27 13:59:39 -0400

    build: added custom emails for password reset and identity verification

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+    modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@vue-email/nuxt"],
     runtimeConfig: {
         auth: {
             verificationCodeExpiryTimeMs: 60 * 5 * 1000, // 5 minutes (ms)
