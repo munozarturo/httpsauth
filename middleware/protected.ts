18cdfd58543215451f21b2e56cbfcc0b32557ad4
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-06-01 21:49:05 -0400

    build: added to, from instead of useRoute

--- middleware/protected.ts
+++ middleware/protected.ts
@@ -1,12 +1,11 @@
-export default defineNuxtRouteMiddleware(async () => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
 	const authStore = useAuthStore();
 
 	const router = useRouter();
-	const route = useRoute();
 
 	onNuxtReady(() => {
 		if (!authStore.context) {
-			router.push(`/auth/signin?callback=${route.fullPath}`);
+			router.push(`/auth/signin?callback=${from.fullPath}`);
 		}
 	});
 });
