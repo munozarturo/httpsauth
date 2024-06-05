585ec58b0ea2c192545772637b3dcf02e766c24b
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-06-05 13:58:20 -0400

    build: sync

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,11 +14,6 @@ export default defineNuxtConfig({
 			path: "~/components/auth/forms",
 			pathPrefix: true,
 		},
-		{
-			path: "~/emails",
-			extensions: ["vue"],
-			pattern: "**/*.vue",
-		},
 	],
 	modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@vue-email/nuxt"],
 	runtimeConfig: {
