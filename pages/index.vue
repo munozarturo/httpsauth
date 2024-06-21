b245289424de13ffd76bde87372a2b6434b8a64a
Arturo Munoz <munoz.arturoroman@gmail.com>
2024-06-21 14:06:12 -0400

    build: consolidate ui

--- pages/index.vue
+++ pages/index.vue
@@ -3,104 +3,133 @@
 		class="h-full flex flex-col space-y-6 px-8 lg:px-32 py-4 lg:pt-16 text-xl"
 	>
 		<Logo type="long" />
-		<div class="space-y-8">
-			<div class="space-y-4">
-				<div class="space-y-2">
-					<p class="pl-4">Let me make this brief...</p>
-					<ul class="pl-10 list-disc">
-						<li>
-							I am
-							<a
-								href="https://munozarturo.com/me"
-								target="_blank"
-								rel="noopener noreferrer"
-								class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-gray-100 text-gray-800 hover:bg-gray-200 transition-colors"
-							>
-								<span class="mr-1">@</span>
-								Arturo
-							</a>
-						</li>
-						<li>I like web development</li>
-						<li>
-							I have always wanted to implement authentication
-							from scratch.
-						</li>
-					</ul>
-					<p class="pl-4">
-						This is my implementation of authentication from
+		<div class="space-y-4">
+			<div class="space-y-2">
+				<p class="pl-4">Let me make this brief...</p>
+				<ul class="pl-10 list-disc">
+					<li>
+						I am
+						<a
+							href="https://munozarturo.com/me"
+							target="_blank"
+							rel="noopener noreferrer"
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-pink-100 text-pink-800 hover:bg-pink-200 transition-colors"
+						>
+							<span class="mr-1">@</span>
+							Arturo
+						</a>
+					</li>
+					<li>I like web development</li>
+					<li>
+						I have always wanted to implement authentication from
 						scratch.
-					</p>
-					<ul class="pl-10 space-y-2">
-						<li>
-							Built with
-							<a
-								href="https://nuxt.com"
-								target="_blank"
-								rel="noopener noreferrer"
-								class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-green-100 text-green-800 hover:bg-green-200 transition-colors"
-							>
-								Nuxt </a
-							>,
-							<a
-								href="https://www.postgresql.org/"
-								target="_blank"
-								rel="noopener noreferrer"
-								class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors"
-							>
-								PostgreSQL </a
-							>, and
-							<a
-								href="https://aws.amazon.com/ses/"
-								target="_blank"
-								rel="noopener noreferrer"
-								class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-orange-100 text-orange-800 hover:bg-orange-200 transition-colors"
-							>
-								Amazon SES </a
-							>.
-						</li>
-						<li>
-							Hosted on
-							<a
-								href="https://vercel.com"
-								target="_blank"
-								rel="noopener noreferrer"
-								class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-black text-white hover:bg-gray-800 transition-colors"
-							>
-								Vercel
-							</a>
-							and
-							<a
-								href="https://github.com/munozarturo/httpsauth"
-								target="_blank"
-								rel="noopener noreferrer"
-								class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-black text-white hover:bg-gray-800 transition-colors"
-							>
-								GitHub </a
-							>.
-						</li>
-					</ul>
-					<p class="pl-4">
-						Try it out by
-						<a class="underline" href="/auth/signup">signing up</a>.
-					</p>
-				</div>
+					</li>
+				</ul>
+				<p class="pl-4">
+					This is my implementation of authentication from scratch.
+				</p>
+				<ul class="pl-10 space-y-2">
+					<li>
+						Built with
+						<a
+							href="https://nuxt.com"
+							target="_blank"
+							rel="noopener noreferrer"
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-green-100 text-green-800 hover:bg-green-200 transition-colors"
+						>
+							Nuxt </a
+						>,
+						<a
+							href="https://www.postgresql.org/"
+							target="_blank"
+							rel="noopener noreferrer"
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-blue-100 text-blue-800 hover:bg-blue-200 transition-colors"
+						>
+							PostgreSQL </a
+						>, and
+						<a
+							href="https://aws.amazon.com/ses/"
+							target="_blank"
+							rel="noopener noreferrer"
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-orange-100 text-orange-800 hover:bg-orange-200 transition-colors"
+						>
+							Amazon SES </a
+						>.
+					</li>
+					<li>
+						Hosted on
+						<a
+							href="https://vercel.com"
+							target="_blank"
+							rel="noopener noreferrer"
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-black text-white hover:bg-gray-800 transition-colors"
+						>
+							Vercel
+						</a>
+						and
+						<a
+							href="https://github.com/munozarturo/httpsauth"
+							target="_blank"
+							rel="noopener noreferrer"
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-black text-white hover:bg-gray-800 transition-colors"
+						>
+							GitHub </a
+						>.
+					</li>
+				</ul>
+				<p class="pl-4">
+					Try it out by
+					<a class="underline" href="/auth/signup">signing up</a>.
+				</p>
 			</div>
-			<div
-				v-if="stats"
-				class="w-full md:w-4/5 grid grid-cols-1 md:grid-cols-5 gap-4 px-4 md:px-12"
-			>
-				<div
-					v-for="(stat, key) in stats"
-					:key="key"
-					class="flex flex-row md:flex-col items-center md:items-stretch justify-between md:justify-center bg-black rounded-lg text-white p-4 space-x-4 md:space-x-0 md:space-y-2 h-24 md:h-32"
-				>
-					<div class="flex items-center justify-center md:flex-grow">
-						<p class="text-3xl font-bold">{{ stat }}</p>
-					</div>
-					<div class="flex items-center justify-center md:h-12">
-						<p class="text-center">{{ formatLabel(key) }}</p>
-					</div>
-				</div>
+			<div v-if="stats" class="w-full">
+				<p class="pl-4">Currently,</p>
+				<ul class="list-disc pl-10">
+					<li class="leading-relaxed">
+						There {{ stats.activeSessions == 1 ? "is" : "are" }}
+						<span
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-blue-100 text-blue-800"
+						>
+							{{ stats.activeSessions }}
+						</span>
+						active session{{ stats.activeSessions == 1 ? "" : "s" }}
+						and
+						<span
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-red-100 text-red-800"
+						>
+							{{ stats.closedSessions }}
+						</span>
+						closed session{{
+							stats.closedSessions == 1 ? "" : "s"
+						}}.
+					</li>
+					<li class="leading-relaxed">
+						The platform has
+						<span
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-green-100 text-green-800"
+						>
+							{{ stats.registeredUsers }}
+						</span>
+						registered user{{
+							stats.registeredUsers == 1 ? "" : "s"
+						}},
+						<span
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-purple-100 text-purple-800"
+						>
+							{{ stats.verifiedUsers }}
+						</span>
+						of which are verified.
+					</li>
+					<li class="leading-relaxed">
+						Users have reset their passwords
+						<span
+							class="inline-flex items-center px-2 py-1 rounded-lg text-sm font-bold bg-red-100 text-red-800"
+						>
+							{{ stats.passwordResets }}
+						</span>
+						time{{ stats.passwordResets == 1 ? "" : "s" }} in total.
+					</li>
+				</ul>
 			</div>
 		</div>
 	</div>
